5b4ae453b9004631c6b7a4d23682c655b2f8af3d
dmedser <dmedser.mephi@gmail.com>
2018-04-12 10:59:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,33 +24,30 @@ const char *wifi_connection_str = "AT+CWJAP_DEF=\"Columbia 3.0\",\"allisinvain\"
  */
 const char *udp_pc_connection_str = "AT+CIPSTART=1,\"UDP\",\"192.168.1.40\",69,69,2\r\n";
      
-#define SHIFT_BUF_LEN                       9
-#define TFTP_OPCODE_NONE                    0x0000
-#define TFTP_OPCODE_RDREQ                   0x0100
-#define TFTP_OPCODE_WRREQ                   0x0200
-#define TFTP_OPCODE_DATA                    0x0300
-#define TFTP_OPCODE_ACK                     0x0400
-#define TFTP_OPCODE_ERROR                   0x0500
-#define CR_NL                               0x0A0D
-#define CR_NL_LEN                           2
-#define IHEX_START_CODE                     ':'
-#define IHEX_RECORD_TYPE_DATA               0x00
-#define IHEX_RECORD_TYPE_EOF                0x01
-#define IHEX_SERVICE_CHAR_COUNT             13
-#define TFTP_PCK_MAX_LEN                    516
-#define IHEX_MAX_C_LEN                      45
-#define BOOTLOADER_START_ADDRESS            0x7000
-#define APP_START_ADDRESS                   0x0000
-
-#define IHEX_MAX_LEN			    16
-
-void     (*jmp_to_app)(void) = APP_START_ADDRESS;
-
-
+#define CR_NL                           0x0A0D
+#define CR_NL_LEN                       2
+#define SHIFT_BUF_LEN                   9
+#define TFTP_OPCODE_NONE                0x0000
+#define TFTP_OPCODE_RDREQ               0x0100
+#define TFTP_OPCODE_WRREQ               0x0200
+#define TFTP_OPCODE_DATA                0x0300
+#define TFTP_OPCODE_ACK                 0x0400
+#define TFTP_OPCODE_ERROR               0x0500
+#define TFTP_PCK_MAX_LEN                516
+#define IHEX_START_CODE                 ':'
+#define IHEX_RECORD_TYPE_DATA           0x00
+#define IHEX_RECORD_TYPE_EOF            0x01
+#define IHEX_SERVICE_CHAR_COUNT         13
+#define IHEX_MAX_LEN			16
+#define IHEX_MAX_C_LEN                  45
+#define BOOTLOADER_START_ADDRESS        0x7000
+#define APP_START_ADDRESS               0x0000
 #define F_CPU				(16000000UL)
 #define F_TIMER1			(F_CPU / 1024)
 #define TICKS_IN_SEC			(F_TIMER1)
 #define BOOT_TIMEOUT_SEC		10
+
+void (*jmp_to_app)(void) = APP_START_ADDRESS;
 
 /* Счетчик секунд таймаута выхода из программы бутлоадера */
 uint8_t sec_cntr = 0;
@@ -63,7 +60,6 @@ uint32_t pg_addr = 0;
 /* Резервный буфер для хранения строки длиной до 16-ти байт */
 uint8_t  pg_str_reserve_buf[IHEX_MAX_LEN];
 uint8_t  pg_str_reserve_buf_idx = 0;
-
 
 typedef enum {
     BOOT_STATE_CTRL,
@@ -85,7 +81,6 @@ typedef struct {
   uint8_t  crc; 
 } ihex_str_t;
 
-
 /* Сдвиговый буфер для приема символов от ESP8266 */
 class shift_buf {
 public:
@@ -96,7 +91,6 @@ public:
     void clr();
 };
 
-
 shift_buf      sb;
 
 boot_state_t   boot_state;
@@ -105,7 +99,7 @@ uint8_t        tftp_pck[TFTP_PCK_MAX_LEN];
 uint16_t       tftp_pck_len = 0;
 uint8_t *      tftp_pck_ptr;
 
-bool tftp_transfer_is_started = false;
+bool 	       tftp_transfer_is_started = false;
 
 ihex_str_t     ihex_str = {/* is_correct */      false,
                            /* len_is_known */    false,
@@ -116,15 +110,13 @@ ihex_str_t     ihex_str = {/* is_correct */      false,
                            /* addr */            0,
                            /* crc */             0};
 
-
 /* Количество непроверенных байт в текущем принятом tftp пакете */
 uint16_t tftp_pck_unchecked = 0;
-/* Количество символов ihex строки, которые отсутсвуют в данном tftp пакете */
+/* Количество символов ihex строки, которые отсутсвуют в текущем принятом tftp пакете */
 uint8_t  tftp_ihex_remainder = 0;
 /* Резервная ihex строка для дозаполнения из текущего принятого tftp пакета, 
    если последняя ihex строка предыдущего пакета была неполной */
 uint8_t  tftp_ihex_reserve_buf[IHEX_MAX_C_LEN];
-
 
 
 void uart_init() {
@@ -135,6 +127,7 @@ void uart_init() {
     UCSR0C = (0 << USBS0) | (3 << UCSZ00);
     UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 }
+
 
 void uart_deinit() {
     UBRR0H = 0;
@@ -149,42 +142,43 @@ void uart_send_byte(uint8_t data) {
     UDR0 = data;
 }
 
+
 uint8_t uart_receive() {
     while (!(UCSR0A & (1 << RXC0)));
     return UDR0;
 }
 
+
 bool uart_byte_is_received() {
     return (UCSR0A & (1 << RXC0));
 }
+
 
 uint8_t uart_get_received_byte() {
     return UDR0;
 }
 
 
-
-
 void boot_timeout_timer_init() {
-	TCCR1B |= (1 << WGM12);	 // Clear on compare match
-	OCR1A = TICKS_IN_SEC;
-	TCCR1B |= (1 << CS12) | (1 << CS10); // ps = 1024 
+    TCCR1B |= (1 << WGM12);	 // Clear on compare match
+    OCR1A = TICKS_IN_SEC;
+    TCCR1B |= (1 << CS12) | (1 << CS10); // ps = 1024 
 }
 
 
 bool boot_timeout_is_expired() {
-	if(TIFR1 & (1 << OCF1A)) {
-		TIFR1 |= (1 << OCF1A); // Clear flag
-		sec_cntr++;
-	}
-	return (sec_cntr == BOOT_TIMEOUT_SEC);
+    if(TIFR1 & (1 << OCF1A)) {
+	TIFR1 |= (1 << OCF1A); // Clear flag
+	sec_cntr++;
+    }
+    return (sec_cntr == BOOT_TIMEOUT_SEC);
 }
 
 
 void boot_timeout_timer_deinit() {
-	TCCR1B = 0;
-	OCR1A = 0;
-	TIFR1 |= (1 << OCF1A);
+    TCCR1B = 0;
+    OCR1A = 0;
+    TIFR1 |= (1 << OCF1A);
 }
 
 
@@ -201,6 +195,7 @@ void shift_buf::add_lsh(char val) {
     
     buf[SHIFT_BUF_LEN - 1] = val;
 }
+
 
 bool shift_buf::is_updated() {
     if(uart_byte_is_received()) {
@@ -220,6 +215,7 @@ bool shift_buf::contains(char *bytes, uint8_t bytes_num) {
     return true;
 }
 
+
 void shift_buf::clr() {
     memset(buf, 0, SHIFT_BUF_LEN);
 }
@@ -231,6 +227,7 @@ void esp_send_str(const char *str) {
     }
 }
 
+
 void esp_wait_for_str(const char *str, const char *cmd = NULL) {
     if(cmd != NULL) {
         esp_send_str(cmd);
@@ -241,6 +238,7 @@ void esp_wait_for_str(const char *str, const char *cmd = NULL) {
         i = (uart_receive() == str[i]) ? (i + 1) : 0;
     }
 }
+
 
 void esp_init() {
     /* ESP8266 Instruction Set
@@ -284,9 +282,8 @@ bool tftp_wrreq_is_accepted() {
 }
 
 uint16_t swap_endianess(uint16_t val) {
-	return (val << 8) | (val >> 8);
+    return (val << 8) | (val >> 8);
 }
-
 
 
 void tftp_send_ack() {
@@ -296,14 +293,15 @@ void tftp_send_ack() {
     tftp_blk_num++;
 }
 
+
 uint8_t hb(uint16_t val) {
-	return (uint8_t)(val >> 8);
+    return (uint8_t)(val >> 8);
 }
+
 
 uint8_t lb(uint16_t val) {
-	return (uint8_t)val;
+    return (uint8_t)val;
 }
-
 
 
 bool tftp_data_pck_is_accepted() {
@@ -311,9 +309,11 @@ bool tftp_data_pck_is_accepted() {
     return sb.contains((char *)tftp_data_pck_header, sizeof(tftp_data_pck_header) + 1);
 }
 
+
 bool tftp_transfer_is_completed() {
     return (ihex_str.is_eof && sb.contains("OK", strlen("OK")));      
 }
+
 
 uint16_t get_tftp_data_pck_len() {
     uint16_t result = 0;
@@ -332,7 +332,6 @@ uint16_t get_tftp_data_pck_len() {
 }
 
 
-
 void atmega_pwr_on() {
     DDRC &= ~(1 << PORTC2);         // PC2 на вход для чтения PWR_GET_DI
     if(!(PORTC & (1 << PORTC2))) {  // Если 0 на PC2
@@ -346,6 +345,7 @@ void atmega_pwr_on() {
 uint8_t ctox(uint16_t c) {
     return ((hb(c) > 0x3F) ? (hb(c) - 0x37) : (hb(c) & 0x0F)) | (((lb(c) > 0x3F) ? (lb(c) - 0x37) : (lb(c) & 0x0F)) << 4);
 }
+
 
 uint8_t get_hex_byte(uint8_t *ptr) {
     return ctox(*(uint16_t *)ptr);
@@ -361,40 +361,40 @@ void get_tftp_pck() {
 
 
 bool ihex_str_is_correct() {
-	uint8_t addr_h = get_hex_byte(tftp_pck_ptr);
-	tftp_pck_ptr += sizeof(uint16_t);
-	uint8_t addr_l = get_hex_byte(tftp_pck_ptr);
-	uint16_t addr_hl = ((uint16_t)addr_h << 8) | (uint16_t)addr_l;
-	bool mb_eof = ((addr_hl == 0) && ihex_str.is_last_in_pck);
-	if(!mb_eof) {
-		if(addr_hl != ihex_str.addr)  {
-			return false;
-		}
-	}
-	tftp_pck_ptr += sizeof(uint16_t);
-	ihex_str.addr += ihex_str.len;
-	ihex_str.crc += addr_h;
-	ihex_str.crc += addr_l;
-	uint8_t ihex_str_record_type = get_hex_byte(tftp_pck_ptr);
-	
-	if(mb_eof && (ihex_str_record_type == IHEX_RECORD_TYPE_EOF)) {
-		ihex_str.is_eof = true;
-	}
-	else if(ihex_str_record_type != IHEX_RECORD_TYPE_DATA) {
+    uint8_t addr_h = get_hex_byte(tftp_pck_ptr);
+    tftp_pck_ptr += sizeof(uint16_t);
+    uint8_t addr_l = get_hex_byte(tftp_pck_ptr);
+    uint16_t addr_hl = ((uint16_t)addr_h << 8) | (uint16_t)addr_l;
+    bool mb_eof = ((addr_hl == 0) && ihex_str.is_last_in_pck);
+    if(!mb_eof) {
+	if(addr_hl != ihex_str.addr)  {
 		return false;
 	}
+    }
+    tftp_pck_ptr += sizeof(uint16_t);
+    ihex_str.addr += ihex_str.len;
+    ihex_str.crc += addr_h;
+    ihex_str.crc += addr_l;
+    uint8_t ihex_str_record_type = get_hex_byte(tftp_pck_ptr);
+
+    if(mb_eof && (ihex_str_record_type == IHEX_RECORD_TYPE_EOF)) {
+	ihex_str.is_eof = true;
+    }
+    else if(ihex_str_record_type != IHEX_RECORD_TYPE_DATA) {
+	return false;
+    }
+    tftp_pck_ptr += sizeof(uint16_t);
+    ihex_str.crc += ihex_str_record_type;
+
+    for(uint8_t i = 0; i < ihex_str.len; i++) {
+	uint8_t b = get_hex_byte(tftp_pck_ptr);
+	pg_str_reserve_buf[i] = b;
+	ihex_str.crc += b;
 	tftp_pck_ptr += sizeof(uint16_t);
-	ihex_str.crc += ihex_str_record_type;
-	
-	for(uint8_t i = 0; i < ihex_str.len; i++) {
-		uint8_t b = get_hex_byte(tftp_pck_ptr);
-		pg_str_reserve_buf[i] = b;
-		ihex_str.crc += b;
-		tftp_pck_ptr += sizeof(uint16_t);
-	}
-	ihex_str.crc += get_hex_byte(tftp_pck_ptr);
-	tftp_pck_ptr += 2 * sizeof(uint16_t);
-	return (ihex_str.crc == 0);
+    }
+    ihex_str.crc += get_hex_byte(tftp_pck_ptr);
+    tftp_pck_ptr += 2 * sizeof(uint16_t);
+    return (ihex_str.crc == 0);
 }
 
 
@@ -445,6 +445,7 @@ void erase_app_space() {
     pg_addr = 0;
 }
 
+
 void boot_program_page() {
     eeprom_busy_wait();
     boot_page_erase(pg_addr);
@@ -471,139 +472,139 @@ void boot_program_page() {
 void setup() {
     atmega_pwr_on();
     cli();
-	uart_init();
+    uart_init();
     esp_init();
-	boot_timeout_timer_init();
-	boot_state = BOOT_STATE_CTRL;
+    boot_timeout_timer_init();
+    boot_state = BOOT_STATE_CTRL;
 }
 
 
 void loop() {
-	while(1) {
-		switch(boot_state) {
-		case BOOT_STATE_CTRL: 
-			if(!tftp_transfer_is_started) {
-				if(boot_timeout_is_expired()) {
-					boot_state = BOOT_STATE_COMPLETE;
-					break;
-				}
-			}	
-			if(sb.is_updated()) {
-				if(tftp_wrreq_is_accepted()) {
-					tftp_transfer_is_started = true;
-					boot_timeout_timer_deinit();
-					sb.clr();
-					erase_app_space();
-					boot_state = BOOT_STATE_ACK;
-				}
-				else if(tftp_data_pck_is_accepted()) {
-					tftp_pck_len = get_tftp_data_pck_len();
-					sb.clr();
-					boot_state = BOOT_STATE_DATA;
-				}
-				else if(tftp_transfer_is_completed()) {
-					boot_state = BOOT_STATE_COMPLETE;
-				}
+    while(1) {
+	switch(boot_state) {
+	case BOOT_STATE_CTRL: 
+		if(!tftp_transfer_is_started) {
+			if(boot_timeout_is_expired()) {
+				boot_state = BOOT_STATE_COMPLETE;
+				break;
 			}
-			break;
-		case BOOT_STATE_ACK:            
-			tftp_send_ack();
-			boot_state = BOOT_STATE_CTRL;
-			break;
-		case BOOT_STATE_DATA:
-			get_tftp_pck();
-			if(ihex_str.is_full) {
-				tftp_pck_ptr = &tftp_pck[0];  
+		}	
+		if(sb.is_updated()) {
+			if(tftp_wrreq_is_accepted()) {
+				tftp_transfer_is_started = true;
+				boot_timeout_timer_deinit();
+				sb.clr();
+				erase_app_space();
+				boot_state = BOOT_STATE_ACK;
+			}
+			else if(tftp_data_pck_is_accepted()) {
+				tftp_pck_len = get_tftp_data_pck_len();
+				sb.clr();
+				boot_state = BOOT_STATE_DATA;
+			}
+			else if(tftp_transfer_is_completed()) {
+				boot_state = BOOT_STATE_COMPLETE;
+			}
+		}
+		break;
+	case BOOT_STATE_ACK:            
+		tftp_send_ack();
+		boot_state = BOOT_STATE_CTRL;
+		break;
+	case BOOT_STATE_DATA:
+		get_tftp_pck();
+		if(ihex_str.is_full) {
+			tftp_pck_ptr = &tftp_pck[0];  
+		}
+		else {
+			/* Дозаполнить ihex строку в резервном буфере, 
+			   если в предыдущем пакете она была принята не полностью */
+			uint8_t i = tftp_pck_unchecked;
+			/* Количество символов в ihex строке */
+			uint8_t ihex_str_c_len = 0;
+			if(ihex_str.len_is_known) {
+				ihex_str_c_len = ihex_str.len*(sizeof(uint16_t)) + IHEX_SERVICE_CHAR_COUNT;                    
 			}
 			else {
-				/* Дозаполнить ihex строку в резервном буфере, 
-				   если в предыдущем пакете она была принята не полностью */
-				uint8_t i = tftp_pck_unchecked;
-				/* Количество символов в ihex строке */
-				uint8_t ihex_str_c_len = 0;
-				if(ihex_str.len_is_known) {
-					ihex_str_c_len = ihex_str.len*(sizeof(uint16_t)) + IHEX_SERVICE_CHAR_COUNT;                    
-				}
-				else {
-					tftp_ihex_reserve_buf[i] = tftp_pck[0];
-					tftp_ihex_reserve_buf[i + 1] = tftp_pck[1];
-					ihex_str_c_len = get_hex_byte(&tftp_ihex_reserve_buf[1])*(sizeof(uint16_t)) + IHEX_SERVICE_CHAR_COUNT;
-				}
-				for(uint8_t j = 0; i < ihex_str_c_len; i++, j++) {
-					tftp_ihex_reserve_buf[i] = tftp_pck[j];           
-				}
-				tftp_ihex_remainder = ihex_str_c_len - tftp_pck_unchecked; 
-				tftp_pck_ptr = &tftp_ihex_reserve_buf[0];
+				tftp_ihex_reserve_buf[i] = tftp_pck[0];
+				tftp_ihex_reserve_buf[i + 1] = tftp_pck[1];
+				ihex_str_c_len = get_hex_byte(&tftp_ihex_reserve_buf[1])*(sizeof(uint16_t)) + IHEX_SERVICE_CHAR_COUNT;
 			}
-             
-			tftp_pck_unchecked = tftp_pck_len;  
-        
-			while(1) {
-				get_ihex_str_status();
-				/* Если ihex строка была дозаполнена в резервнм буфере, 
-				   перевести указатель tftp пакета на новую ihex строку */
-				if(tftp_ihex_remainder > 0) {
-					tftp_pck_ptr = &tftp_pck[0] + tftp_ihex_remainder;
-					tftp_ihex_remainder = 0;
+			for(uint8_t j = 0; i < ihex_str_c_len; i++, j++) {
+				tftp_ihex_reserve_buf[i] = tftp_pck[j];           
+			}
+			tftp_ihex_remainder = ihex_str_c_len - tftp_pck_unchecked; 
+			tftp_pck_ptr = &tftp_ihex_reserve_buf[0];
+		}
+
+		tftp_pck_unchecked = tftp_pck_len;  
+
+		while(1) {
+			get_ihex_str_status();
+			/* Если ihex строка была дозаполнена в резервнм буфере, 
+			   перевести указатель tftp пакета на новую ihex строку */
+			if(tftp_ihex_remainder > 0) {
+				tftp_pck_ptr = &tftp_pck[0] + tftp_ihex_remainder;
+				tftp_ihex_remainder = 0;
+			}
+			if(ihex_str.is_correct) {
+
+				/* Добавить строку к странице, если страница заполнена, приостановить запонение */
+				while((pg_str_reserve_buf_idx < ihex_str.len) && (pg_idx < SPM_PAGESIZE)) {
+					pg[pg_idx++] = pg_str_reserve_buf[pg_str_reserve_buf_idx++];
 				}
-				if(ihex_str.is_correct) {
 
-					/* Добавить строку к странице, если страница заполнена, приостановить запонение */
-					while((pg_str_reserve_buf_idx < ihex_str.len) && (pg_idx < SPM_PAGESIZE)) {
-						pg[pg_idx++] = pg_str_reserve_buf[pg_str_reserve_buf_idx++];
-					}
+				/* Если страница заполнена или конец файла */
+				if(ihex_str.is_eof || (pg_idx == SPM_PAGESIZE)) {
+					boot_program_page(); 
+					pg_addr += pg_idx; 
+					pg_idx = 0;
+				} 
 
-					/* Если страница заполнена или конец файла */
-					if(ihex_str.is_eof || (pg_idx == SPM_PAGESIZE)) {
-						boot_program_page(); 
-						pg_addr += pg_idx; 
-						pg_idx = 0;
-					} 
+				/* Если в предыдущую страницу строка поместилась неполностью, поместить оставшуюся часть 
+				   строки в новую страницу */
+				while(pg_str_reserve_buf_idx < ihex_str.len) {
+					pg[pg_idx++] = pg_str_reserve_buf[pg_str_reserve_buf_idx++];
+				} 
 
-					/* Если в предыдущую страницу строка поместилась неполностью, поместить оставшуюся часть 
-       					   строки в новую страницу */
-					while(pg_str_reserve_buf_idx < ihex_str.len) {
-						pg[pg_idx++] = pg_str_reserve_buf[pg_str_reserve_buf_idx++];
-					} 
+				pg_str_reserve_buf_idx = 0;
 
-					pg_str_reserve_buf_idx = 0;
-					
-					if(ihex_str.is_last_in_pck) {
-						boot_state = BOOT_STATE_ACK;
-						break;
-					}
-				}
-				else if(ihex_str.is_last_in_pck && !ihex_str.is_full) {
-					/* Поместить принятую часть ihex строки в резервный буфер  */
-					uint8_t *ptr = tftp_pck_ptr - 1;
-					for(uint8_t i = 0; i < tftp_pck_unchecked; i++) {
-						tftp_ihex_reserve_buf[i] = *ptr++;
-					}
+				if(ihex_str.is_last_in_pck) {
 					boot_state = BOOT_STATE_ACK;
-					break; 
-				}
-				else {
-					boot_state = BOOT_STATE_ERROR; 
 					break;
 				}
 			}
-			break;
-		case BOOT_STATE_COMPLETE:
-			uart_deinit();	
-			jmp_to_app();
-			break;
-		case BOOT_STATE_ERROR:
-			erase_app_space(); 
-			break;
-		default:
-			break;
-	   }
-   }
+			else if(ihex_str.is_last_in_pck && !ihex_str.is_full) {
+				/* Поместить принятую часть ihex строки в резервный буфер  */
+				uint8_t *ptr = tftp_pck_ptr - 1;
+				for(uint8_t i = 0; i < tftp_pck_unchecked; i++) {
+					tftp_ihex_reserve_buf[i] = *ptr++;
+				}
+				boot_state = BOOT_STATE_ACK;
+				break; 
+			}
+			else {
+				boot_state = BOOT_STATE_ERROR; 
+				break;
+			}
+		}
+		break;
+	case BOOT_STATE_COMPLETE:
+		uart_deinit();	
+		jmp_to_app();
+		break;
+	case BOOT_STATE_ERROR:
+		erase_app_space(); 
+		break;
+	default:
+		break;
+   	}
+    }
 }
 
 
 void main() {
-	setup();
-	loop();
+    setup();
+    loop();
 }
 
